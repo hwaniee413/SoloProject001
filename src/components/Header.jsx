52bd8bff070f536db3e8b4ac770db6846eca17ad
hwaniee413 <hwaniee413@gmail.com>
2023-05-12 17:34:50 +0900

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Header = () =>{
+
+    return (
+        <div>
+            <h1> [ This is 헤더(jsx) ] </h1>
+        </div>
+   
+    )
+
+}
+export default Header;
